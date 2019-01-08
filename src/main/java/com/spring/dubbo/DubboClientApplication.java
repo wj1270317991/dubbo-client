59ed550f285ee703586af86d5ae949d320f8f278
wangjun <1270317991@qq.com>
2019-01-08 23:30:49 +0800

    first commit

---
+++ src/main/java/com/spring/dubbo/DubboClientApplication.java
@@ -0,0 +1,14 @@
+package com.spring.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboClientApplication.class, args);
+	}
+
+}
+
